551681ebd4ba9f6d08c068045e302d7f253a3492
Deniss Simonaits <dsimonaits@gmail.com>
2022-11-27 22:49:01 +0200

    Create ContactList.styled.js

---
+++ src/components/ContactList/ContactList.styled.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const List = styled.ul`
+  display: flex;
+  flex-flow: column;
+  list-style: none;
+  max-height: 300px;
+  overflow-y: auto;
+`;
+
+export const ListItem = styled.li`
+  width: 400px;
+  padding-left: 10px;
+  padding-right: 10px;
+  border: 1px solid black;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: white;
+  background-color: black;
+
+  &:not(:last-child) {
+    margin-bottom: 5px;
+  }
+`;
+
+export const Btn = styled.button`
+  cursor: pointer;
+  padding: 5px;
+  border: 1px solid white;
+  border-radius: 5px;
+  color: white;
+  background-color: black;
+  &:hover {
+    color: black;
+    background-color: white;
+  }
+`;
+
+export const Number = styled.span`
+  margin-left: 10px;
+`;
