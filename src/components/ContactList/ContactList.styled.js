8fa2dffcd205bbebefd8482968d3e4700a907310
Deniss Simonaits <dsimonaits@gmail.com>
2022-11-29 22:23:34 +0200

    Update ContactList.styled.js

--- src/components/ContactList/ContactList.styled.js
+++ src/components/ContactList/ContactList.styled.js
@@ -7,36 +7,3 @@ export const List = styled.ul`
   max-height: 300px;
   overflow-y: auto;
 `;
-
-export const ListItem = styled.li`
-  width: 400px;
-  padding-left: 10px;
-  padding-right: 10px;
-  border: 1px solid black;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  color: white;
-  background-color: black;
-
-  &:not(:last-child) {
-    margin-bottom: 5px;
-  }
-`;
-
-export const Btn = styled.button`
-  cursor: pointer;
-  padding: 5px;
-  border: 1px solid white;
-  border-radius: 5px;
-  color: white;
-  background-color: black;
-  &:hover {
-    color: black;
-    background-color: white;
-  }
-`;
-
-export const Number = styled.span`
-  margin-left: 10px;
-`;
