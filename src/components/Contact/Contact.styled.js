61d89b9255b2bb5096f62f8aea39365839d5121a
Deniss Simonaits <dsimonaits@gmail.com>
2022-11-29 22:35:50 +0200

    Update Contact.styled.js

--- src/components/Contact/Contact.styled.js
+++ src/components/Contact/Contact.styled.js
@@ -1,21 +1,5 @@
 import styled from 'styled-components';
 
-export const ListItem = styled.li`
-  width: 400px;
-  padding-left: 10px;
-  padding-right: 10px;
-  border: 1px solid black;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  color: white;
-  background-color: black;
-
-  &:not(:last-child) {
-    margin-bottom: 5px;
-  }
-`;
-
 export const Btn = styled.button`
   cursor: pointer;
   padding: 5px;
