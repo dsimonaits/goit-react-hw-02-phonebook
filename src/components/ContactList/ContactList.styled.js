37489c81062b4d1432e107ff04242da14a66d277
Deniss Simonaits <dsimonaits@gmail.com>
2022-11-29 22:35:55 +0200

    Update ContactList.styled.js

--- src/components/ContactList/ContactList.styled.js
+++ src/components/ContactList/ContactList.styled.js
@@ -7,3 +7,19 @@ export const List = styled.ul`
   max-height: 300px;
   overflow-y: auto;
 `;
+
+export const ListItem = styled.li`
+  width: 400px;
+  padding-left: 10px;
+  padding-right: 10px;
+  border: 1px solid black;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: white;
+  background-color: black;
+
+  &:not(:last-child) {
+    margin-bottom: 5px;
+  }
+`;
